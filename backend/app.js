ca793c160d029cecb51964ea8af26f3ed3b664f7
Jelena Bojovic <jelenabojovic981@gmail.com>
2022-04-12 13:54:25 +0200

    connection to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,17 @@
 // Recupération Express et mongoose
 const express = require('express');
 
+const mongoose = require('mongoose');
+
+
 const app = express();
+// connection à la base données
+mongoose.connect('mongodb+srv://jelenabojovic:2Ggh1ECn5YKtjYlc@cluster0.r2oqh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use ((req,res) => {
     res.json({ message: 'Votre requête a bien été reçue !'});
 })
