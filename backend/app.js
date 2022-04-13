4fb53cbb9b6e494d2835f870ad089bfb90b46545
Jelena Bojovic <jelenabojovic981@gmail.com>
2022-04-13 11:54:19 +0200

    added .env

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,30 @@
 const express = require('express');
 
 const mongoose = require('mongoose');
+const dotenv = require("dotenv");
 
 
 const app = express();
+
+//Variable d'environnement
+dotenv.config();
+
 // connection à la base données
-mongoose.connect('mongodb+srv://jelenabojovic:2Ggh1ECn5YKtjYlc@cluster0.r2oqh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect(process.env.MONGO_URI,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+   next();
+ });
+ 
 
 app.use ((req,res) => {
     res.json({ message: 'Votre requête a bien été reçue !'});
